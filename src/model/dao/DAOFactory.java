e846d1d95284935720719add734bb35202c4b559
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-04 23:24:35 -0300

    findById implementation

--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDAO_JDBC;
 
 public class DAOFactory {
 
 	public static SellerDAO createSellerDao() {
-		return new SellerDAO_JDBC();
+		return new SellerDAO_JDBC(DB.getConnection());
 	}
 }
