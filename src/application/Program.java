4b258f29620a25c6e2d8fcc275e5f952426ae4df
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-05 01:12:28 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,5 +37,11 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New ID = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Carolina Maria de Jesus");
+		sellerDao.update(seller);
+		System.out.println("Update complete");
 	}
 }
