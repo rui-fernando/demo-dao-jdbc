a5ec6fd34710a3f173fc166f017d053d4c430bd7
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-05 02:42:21 -0300

    insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -21,5 +21,10 @@ public class Program2 {
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller insert ===");
+		Department newDepartment = new Department(null, "Magalu");
+		depDao.insert(newDepartment);
+		System.out.println("Inserted! New ID = " + newDepartment.getId());
 	}
 }
